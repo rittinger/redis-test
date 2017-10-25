70604d1c9b7c704a7d7e8589329dc9ec0a2e1c91
Ron Rittinger <ron.rittinger@kroger.com>
2017-10-24 21:31:47 -0400

    Not quite, but close

--- redis-lettuce/src/main/java/com/example/redislettuce/config/SpringLettuceConfig.java
+++ redis-lettuce/src/main/java/com/example/redislettuce/config/SpringLettuceConfig.java
@@ -8,17 +8,22 @@ import com.lambdaworks.redis.resource.DefaultClientResources;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 public class SpringLettuceConfig {
-  @Value("${redis.host}")
-  private String redisHost;
-
-  @Value("${redis.port}")
-  private int redisPort = 5;
-
-  @Value("${redis.database}")
+  @Value("${spring.redis.database}")
   private int database;
+
+  @Value("${spring.redis.password}")
+  private String password;
+
+  @Value("${spring.redis.sentinel.nodes}")
+  private String nodes;
+
+  @Value("${spring.redis.sentinel.master}")
+  private String master;
 
   @Bean(destroyMethod = "shutdown")
   ClientResources clientResources() {
@@ -27,9 +32,25 @@ public class SpringLettuceConfig {
 
   @Bean(destroyMethod = "shutdown")
   RedisClient redisClient(ClientResources clientResources) {
-    RedisURI redisUri = RedisURI.create(redisHost, redisPort);
-    redisUri.setDatabase(database);
-    return RedisClient.create(clientResources, redisUri);
+    //RedisURI redisUri = RedisURI.create(redisHost, redisPort);
+    //redisUri.setDatabase(database);
+    //return RedisClient.create(clientResources, redisUri);
+\
+    // "redis-sentinel://localhost:26379,localhost:26380/0#mymaster"
+//    RedisURI redisUri = RedisURI.builder()
+//        .withDatabase(database)
+//        .withPassword(password)
+//        .withSentinel(nodes)
+//        .build();
+//
+//    return RedisClient.create(clientResources, redisUri);
+
+    RedisSentinelConfiguration sentinelConfiguration = new RedisSentinelConfiguration()
+        .master(master)
+        .sentinel("localhost", 26379);
+    LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(sentinelConfiguration);
+    lettuceConnectionFactory.setDatabase(database);
+    lettuceConnectionFactory.setPassword(password);
   }
 
   @Bean(destroyMethod = "close")
